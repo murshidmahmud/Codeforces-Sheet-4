93c0b6ab6bbb5285f7c8d3e35f9491971242a469
Murshid Mahmud <murshidmahmud4@gmail.com>
2026-01-29 02:05:44 +0600

    29-01-2026

---
+++ C_Compare.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    string x,y;
+    cin>>x;
+    cin>>y;
+
+    if(x<y){
+        cout<<x;
+    }
+    else{
+        cout<<y;
+    }
+    return 0;
+}
