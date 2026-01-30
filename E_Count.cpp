33fb1edde96e2804c539cbb340887bedce322088
Murshid Mahmud <murshidmahmud4@gmail.com>
2026-01-30 23:31:19 +0600

    31-0132026

---
+++ E_Count.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    string s;
+    cin>>s;
+
+    int a = s[0] -'0';
+    int b = s[1] -'0';
+    int c = s[2] -'0';
+
+    cout<<a+b+c;  
+    return 0;
+}
